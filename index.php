e6661efc727a032af8f18a5c2c6d6254ee3d63c1
Rebecca McFadden <rmcfadden@tamu.edu>
2020-01-22 01:44:33 -0600

    php first page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> <!--from freecodecamp.org-->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src='js/main.js'></script>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<?php
+include $_GET['page'].".html";
+include 'navbar.html';
+//include 'footer.html';
+?>
+</html>
