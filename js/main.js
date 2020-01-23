4a347088d66b2c709b1a6284c83ada312f1b6f6e
Rebecca McFadden <rmcfadden@tamu.edu>
2020-01-22 23:24:54 -0600

    add resume page

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ $(document).ready(function () {
     });
 
 });
+$('.carousel').on('slid.bs.carousel', function () {
+    var carouselData = $(this).data('bs.carousel');
+    var currentIndex = carouselData.getItemIndex(carouselData.$element.find('.item.active'));
+
+    $('.result p')
+        .removeClass('active-p')
+        .eq(currentIndex)
+        .addClass('active-p');
+});
